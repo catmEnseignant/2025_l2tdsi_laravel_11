c8a40bbbaa813f17afbfee88f81e66afc623b92d
diakhala <diakhalakonte97@gmail.com>
2025-10-25 17:52:33 +0000

    rendue du tp

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\WelcomController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/diakhala/konte', [WelcomController::class, 'index']);
