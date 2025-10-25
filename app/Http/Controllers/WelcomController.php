0657e8a6447ad174b8624b9df8a2cd53e96cce10
MARIH <rydiopdrame04@gmail.com>
2025-10-25 17:35:29 +0000

    fisrt commit

---
+++ app/Http/Controllers/WelcomController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WelcomController extends Controller
+{
+    function index1(){
+        return view('welcome');
+    }
+
+    function admin(){
+        return view('admin');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,17 @@
 //use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Routes;
 use App\Http\Controllers\AccueilController;
+use App\Http\Controllers\WelcomController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+//Route::get('/', function () {
+    //return view('welcome');
+//});
 Route::get('/accueil',[AccueilController::class, 'index']);
+Route::get('/tester/index',[WelcomController::class, 'index1']);
+Route::get('/tester/admin',[WelcomController::class, 'admin']);
+
+
 
 // Route :: get('/blog' , function(Request $request) {
 //         return "bonjour";
