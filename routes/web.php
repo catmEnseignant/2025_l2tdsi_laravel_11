a2a2f892f65a01d158f053b73f20de2952b197e0
Fatou Bintou <fatimatoubinetourassoul01@gmail.com>
2025-10-25 18:01:55 +0000

    Création page index et admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
-
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/test/index', [WelcomeController::class, 'index']);
+Route::get('/test/admin', [WelcomeController::class, 'admin']);
