63c2d0cf2d5e29e6f6bf931889589f73005931a7
topsokhna-top <s6695119@gmail.com>
2025-10-26 07:08:32 +0100

    integration template

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/index', [WelcomeController::class, 'index']
+);
