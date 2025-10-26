b49af1cc4806f83c361124f098eaab1d50d9648f
Ibrahima-Khalilou-llah-Sylla <khaliloullahsylla@gmail.com>
2025-10-26 14:42:30 +0000

    ajout de admin page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/khalil/home', [HomeController::class, 'index']);
+Route::get('/admin', [HomeController::class, 'admin']);
