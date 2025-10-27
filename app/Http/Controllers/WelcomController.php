160b50dbe9004f74fdcc5944f8735e85cd89ed31
salimatafall <salimatafall@gmail.com>
2025-10-27 11:40:02 +0000

    ajout du projet

---
+++ app/Http/Controllers/WelcomController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WelcomController extends Controller
+{
+function index () {
+    return view ("index");
+} 
+function admin () {
+    return view ("admin");
+} 
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\WelcomController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/test/index',[WelcomController::class , "index"]);
+
+Route::get('/tester/admin',[WelcomController::class , "admin"]);
