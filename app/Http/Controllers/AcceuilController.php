3d55a38541e6b9d93be11eb1d07d59e94e899e98
moussa04gueye <mousse04gueye@gmail.com>
2025-10-27 21:01:55 +0000

    add template

---
+++ app/Http/Controllers/AcceuilController.php
@@ -0,0 +1,19 @@
+<!-- <?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AcceuilController extends Controller
+{
+    // function index(){
+    //     $age= $_GET['age']; 
+    //     $celiba= true;
+    //     $maries= ['moussa','tine', 'khalil'];
+    //     if($age>18){
+    //         return view('index',compact ('age', 'celiba', 'maries'));
+    //     }else{
+    //         return view('welcome' , compact ('age', 'celiba','maries'));
+    //     }
+    // }
+}
