8e0a04ec7b6b71c4d4fba910d2b12154931d0388
MO-TINE <tinemamadou636@gmail.com>
2025-11-12 01:46:07 +0000

    premier commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AccueilController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Mamadou/Tine', [AccueilController::class, 'index']);
