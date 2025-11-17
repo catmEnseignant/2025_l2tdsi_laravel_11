da331b0f38a035168879ce941ea49df1cc9364b0
khadija <khadymbackethioune1@gmail.com>
2025-11-17 23:54:28 +0000

    VOICI MON TP1 ET MON TP2

---
+++ resources/views/sidebar.blade.php
@@ -0,0 +1,70 @@
+<aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
+  <!--begin::Sidebar Brand-->
+  <div class="sidebar-brand">
+    <!--begin::Brand Link-->
+    <a href="./index.html" class="brand-link">
+      <!--begin::Brand Image-->
+      <img
+        src="./assets/img/AdminLTELogo.png"
+        alt="AdminLTE Logo"
+        class="brand-image opacity-75 shadow"
+      />
+      <!--end::Brand Image-->
+      <!--begin::Brand Text-->
+      <span class="brand-text fw-light">AdminLTE 4</span>
+      <!--end::Brand Text-->
+    </a>
+    <!--end::Brand Link-->
+  </div>
+  <!--end::Sidebar Brand-->
+  
+  <!--begin::Sidebar Wrapper-->
+  <div class="sidebar-wrapper">
+    <nav class="mt-2">
+      <!--begin::Sidebar Menu-->
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
+        <!-- Dashboard -->
+        <li class="nav-item">
+          <a href="./index.html" class="nav-link">
+            <i class="nav-icon bi bi-speedometer2"></i>
+            <p>Dashboard</p>
+          </a>
+        </li>
+        
+        <!-- Médecins -->
+        <li class="nav-item">
+          <a href="./medecins.html" class="nav-link">
+            <i class="nav-icon bi bi-person-badge"></i>
+            <p>Médecins</p>
+          </a>
+        </li>
+        
+        <!-- Départements -->
+        <li class="nav-item">
+          <a href="./departements.html" class="nav-link">
+            <i class="nav-icon bi bi-building"></i>
+            <p>Départements</p>
+          </a>
+        </li>
+        
+        <!-- Services -->
+        <li class="nav-item">
+          <a href="./services.html" class="nav-link">
+            <i class="nav-icon bi bi-hospital"></i>
+            <p>Services</p>
+          </a>
+        </li>
+        
+        <!-- Patients -->
+        <li class="nav-item">
+          <a href="./patients.html" class="nav-link">
+            <i class="nav-icon bi bi-people"></i>
+            <p>Patients</p>
+          </a>
+        </li>
+      </ul>
+      <!--end::Sidebar Menu-->
+    </nav>
+  </div>
+  <!--end::Sidebar Wrapper-->
+</aside>
